5151e2175d63862822afb329db89e0f1d56f0c0b
fadillrama <fadillahrama70@gmail.com>
2024-04-24 13:27:55 +0700

    initial commit

--- src/main/java/com/Assessment/service/NasabahService.java
+++ src/main/java/com/Assessment/service/NasabahService.java
@@ -6,8 +6,6 @@ import com.Assessment.repository.NasabahRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class NasabahService {
     @Autowired
